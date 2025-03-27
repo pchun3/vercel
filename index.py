d91e66bb03a041e6680a482ba19f33adcced03e2
pchun3 <s411087077@gmail.com>
2025-03-27 10:05:52 +0800

    v1_get

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def index():
 	homepage = "<h1>王珮淳Python網頁</h1>"
 	homepage += "<a href=/mis>MIS</a><br>"
 	homepage += "<a href=/today>顯示日期時間</a><br>"
-	homepage += "<a href=/welcome?nick=王珮淳>傳送使用者暱稱</a><br>"
+	homepage += "<a href=/welcome?nick=王珮淳&work=pu>傳送使用者暱稱</a><br>"
 	homepage += "<a href=/account>網頁表單傳值</a><br>"
 	homepage += "<a href=/about>王珮淳簡介網頁</a><br>"
 	return homepage	
@@ -30,7 +30,8 @@ def about():
 @app.route("/welcome", methods=["GET"])
 def welcome():
 	user = request.values.get("nick")
-	return render_template("welcome.html", name=user)
+	w = request.values.get("work")
+	return render_template("welcome.html", name=user, work = w)
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
